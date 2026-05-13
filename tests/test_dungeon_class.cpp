9893a984886fde595b4f78ee7eb86bc2ebff844d
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-05-14 00:03:49 +0300

    Added tests for the Dungeon class

--- tests/test_dungeon_class.cpp
+++ tests/test_dungeon_class.cpp
@@ -1,9 +1,33 @@
 #include <gtest/gtest.h>
+
+#include "bot.h"
 #include "dungeon.h"
 
-TEST(TestTask1, Dungeon_Load_World_Test) {
-    std::string test_file = "in1_success.txt";
+TEST(TestTask1, Dungeon_LoadDungeon_Test_With_Throw) {
+    std::string test_file = "...";
 
     Dungeon dungeon;
     ASSERT_ANY_THROW(dungeon.loadDungeon(test_file));
+}
+
+TEST(TestTask1, Dungeon_LoadDungeon_Test_1) {
+    std::string test_file = "in1_success.txt";
+
+    Dungeon dungeon;
+    dungeon.loadDungeon(test_file);
+
+    auto costs = dungeon.getCosts();
+    auto person = dungeon.getPerson();
+
+    EXPECT_EQ(costs["iron"], DEFAULT_IRON_COST);
+    EXPECT_EQ(costs["gold"], DEFAULT_GOLD_COST);
+    EXPECT_EQ(costs["gems"], DEFAULT_GEMS_COST * 2);
+    EXPECT_EQ(costs["exp"], DEFAULT_EXP_COST);
+
+    EXPECT_EQ(dungeon.getCountPaths(), 6);
+    EXPECT_EQ(dungeon.getCountRooms(), 6);
+
+    EXPECT_EQ(person.getCurrentRoomNumber(), 0);
+    EXPECT_EQ(person.getCurrentCountFoods(), 6);
+    EXPECT_EQ(person.isAlive(), true);
 }
