896b0b8addf0e60172ae4aae96e192b480b5f2a4
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-05-14 14:05:32 +0300

    Specified the file name via command line arguments

--- main.cpp
+++ main.cpp
@@ -5,11 +5,22 @@
 #define INPUT_FILE_PATH "in.txt"
 #endif
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    std::string in_file = INPUT_FILE_PATH;
+
+    if(argc == 2) {
+        in_file = argv[1];
+    }
+
     try {
+        std::cout << "input file: " << in_file << std::endl;
+
         Logger logger;
-        AliseBot bot(INPUT_FILE_PATH);
+        AliseBot bot(in_file);
         bot.start();
+
+        std::cout << "output file: result.txt" << std::endl;
     } catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
